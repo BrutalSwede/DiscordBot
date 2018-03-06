03ae4395f5dfaea8c2fe804540acb9bdf22b7ba6
BrutalSwede <pontus.bua@gmail.com>
2018-03-06 20:25:07 +0100

    Initial project upload

---
+++ DiscordBot/Modules/InfoModule.cs
@@ -0,0 +1,60 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using DiscordBot.Settings;
+
+namespace DiscordBot.Modules
+{
+    class InfoModule : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("shoutout")]
+        [Summary("Shout out a user")]
+        public async Task ShoutOutAsync(SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+
+            await ReplyAsync($"{user.Mention} is a dummy!");
+        }
+
+
+        [Command("newXML")]
+        public async Task NewXml()
+        {
+            await ReplyAsync("Creating new settings xml");
+            SettingsController.Init();
+        }
+
+        [Command("readXml")]
+        public async Task ReadXml()
+        {
+            Settings s = await SettingsController.GetSettingsAsync();
+            await ReplyAsync($"Current Settings:\n" +
+                             $"PostRSS = \t {s.PostRSS}\n" +
+                             $"TestBool = \t {s.TestBool}");
+        }
+
+        [Command("galnet")]
+        [Summary("Enables/Disables Galnet RSS feed")]
+        public async Task rss([Summary("Whether or not the bot will post RSS updates.")]bool setting)
+        {
+            Settings s = await SettingsController.GetSettingsAsync();
+
+            if(setting == s.PostRSS)
+            {
+                await ReplyAsync($"RSS already set to {setting}");
+                return;
+            }
+            else
+            {
+                await ReplyAsync($"Changing RSS setting.");
+                s.PostRSS = setting;
+                SettingsController.SaveSettings(s);
+            }
+
+
+        }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using DiscordBot.Modules;
+using DiscordBot.Settings;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _client.Log += Log;
+            //_client.MessageReceived += MessageRecieved;
+
+            string token = ""; // Get token from config.json
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            await InstallCommandAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+        }
+
+        private async Task InstallCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            // Load command module
+            await _commands.AddModuleAsync<InfoModule>();
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+
+            var context = new SocketCommandContext(_client, message);
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        private async Task MessageRecieved(SocketMessage message)
+        {
+            if(message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg);
+            return Task.CompletedTask;
+        }
+    }
+}
